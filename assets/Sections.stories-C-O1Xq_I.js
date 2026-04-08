ec4d644124e140cbb55d2c694d641c88068286ac
SOLO <solo@example.com>
2026-04-08 13:17:13 +0000

    Updates

--- assets/Sections.stories-C-O1Xq_I.js
+++ assets/Sections.stories-C-O1Xq_I.js
@@ -1,4 +1,4 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-D16BNjX-.js";import{a as n,c as r,i,n as a,o,r as s,s as c,t as l}from"./WhyAttendGrid-CHjzWHVH.js";var u,d,f,p,m,h,g;e((()=>{r(),o(),i(),a(),u=t(),d={title:`Sections/Overview`},f={render:()=>(0,u.jsx)(l,{})},p={render:()=>(0,u.jsx)(n,{})},m={render:()=>(0,u.jsx)(s,{})},h={render:()=>(0,u.jsx)(c,{})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-D16BNjX-.js";import{a as n,c as r,i,n as a,o,r as s,s as c,t as l}from"./WhyAttendGrid-C2ohGv1V.js";var u,d,f,p,m,h,g;e((()=>{r(),o(),i(),a(),u=t(),d={title:`Sections/Overview`},f={render:()=>(0,u.jsx)(l,{})},p={render:()=>(0,u.jsx)(n,{})},m={render:()=>(0,u.jsx)(s,{})},h={render:()=>(0,u.jsx)(c,{})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
   render: () => <WhyAttendGrid />
 }`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
   render: () => <SpeakersGrid />
